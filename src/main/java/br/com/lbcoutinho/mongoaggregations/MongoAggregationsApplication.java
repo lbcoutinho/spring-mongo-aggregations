a3117a883e46071502a6c44a4f097a99735c39ce
Leandro Coutinho <leandro.coutinho@4all.com>
2019-08-09 20:26:08 -0300

    Initial commit

---
+++ src/main/java/br/com/lbcoutinho/mongoaggregations/MongoAggregationsApplication.java
@@ -0,0 +1,13 @@
+package br.com.lbcoutinho.mongoaggregations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoAggregationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoAggregationsApplication.class, args);
+	}
+
+}
